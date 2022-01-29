2c2420427e7f037b1697905607c36bf944fd9871
seventhseal <kamura657@gmail.com>
2022-01-30 00:33:04 +0530

    trial commit 5

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import './App.css'
 import image from './image.png'
 
 const App = () => {
-  const name = 'name'
-  const name2 = 'name2'
-  const name3 = 'name2'
+  // const name = 'name'
+  // const name2 = 'name2'
+  // const name3 = 'name2'
+  // const name4 = 'name2'
   return (
     <div className="heading">
       Hello world
